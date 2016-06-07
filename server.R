4967137e27be59989a776b73ea51fa32888ad9cf
Tony Fregoli <tonyfregoli@gwlonse.local>
2016-06-07 12:56:18 +0100

    db.R check

--- server.R
+++ server.R
@@ -10,108 +10,115 @@ library(shiny)
 library(dplyr)
 library(DT)
 
-source('db.R')
 source("server_config.R")
 source("server_functions.R")
 
-
-# Data 
-refresh_reactive_data <- observe({
-  invalidateLater(REFRESH_INTERVAL * 1000 * 60)  #interval in minutes
-  #invalidateLater(REFRESH_INTERVAL * 1000)      #interval in seconds
-  
-  # PMP_PERFORMANCE_DAILY
-  PMP_PERFORMANCE_DAILY$data <<- pg_select(PMP_PERFORMANCE_DAILY$source)
-
-  # PMP_PERFORMANCE_HOURLY
-  PMP_PERFORMANCE_HOURLY$data <<- pg_select(PMP_PERFORMANCE_HOURLY$source)
-  
-  # BOROUGHS
-  boroughs = distinct(select(PMP_PERFORMANCE_DAILY$data, borough))
-  PMP_BOROUGHS$data <<- boroughs$borough
-  
-  # LAST_UPDATE
-  REFRESH_LAST <<- Sys.time()
-  
-  cat(paste(REFRESH_LAST, "Data has been refreshed", "\n"))
-})
-
-
-
-# Shinyserver
-shinyServer(function(input, output, session) {
-  
-  # Check first run
-  check_firstrun <- reactiveValues(data = TRUE)
-  session$onFlushed(function() {check_firstrun$data <- FALSE}, once = TRUE)
+# db.R
+if (!DB) {
+  shinyServer(function(input, output) {
+  })
+} else {
 
   
-  # CEO Performance
+  # Data 
+  refresh_reactive_data <- observe({
+    invalidateLater(REFRESH_INTERVAL * 1000 * 60)  #interval in minutes
+    #invalidateLater(REFRESH_INTERVAL * 1000)      #interval in seconds
     
-    # Data
-    ceo_performance_data <- reactive({
+    # PMP_PERFORMANCE_DAILY
+    PMP_PERFORMANCE_DAILY$data <<- pg_select(PMP_PERFORMANCE_DAILY$source)
+  
+    # PMP_PERFORMANCE_HOURLY
+    PMP_PERFORMANCE_HOURLY$data <<- pg_select(PMP_PERFORMANCE_HOURLY$source)
     
-      ppd_daterange <- input$ceo_performance_date_ctrl
-      ppd_borough   <- input$ceo_performance_borough_ctrl
-      
-      filter_clause <- list(~attempts_date >= ppd_daterange[1], ~attempts_date <= ppd_daterange[2])
-      if (ppd_borough != "All") filter_clause <- append(filter_clause, ~borough == ppd_borough)
-      
-      ppd_filtered <- filter_(PMP_PERFORMANCE_DAILY$data, .dots = filter_clause) #Standard Evaluation
-      
-      ppd_grouped <- group_by(ppd_filtered, attempt_user)
-      
-      ppd_by_user <- summarise(ppd_grouped,
-                               total_attempts    = sum(total_attempts),
-                               total_engaged     = sum(total_f2f),
-                               total_not_home    = sum(total_not_home),
-                               total_no_property = sum(total_no_proerty_exists),
-                               rate_engaged      = sum(total_f2f)/sum(total_attempts),
-                               rate_not_home     = sum(total_not_home)/sum(total_attempts),
-                               rate_no_property  = sum(total_no_proerty_exists)/sum(total_attempts))
-
-    })
+    # BOROUGHS
+    boroughs = distinct(select(PMP_PERFORMANCE_DAILY$data, borough))
+    PMP_BOROUGHS$data <<- boroughs$borough
     
-    # Chart: Performance Chart
-    output$ceo_performance_performance_chart <- renderPlot({
+    # LAST_UPDATE
+    REFRESH_LAST <<- Sys.time()
+    
+    cat(paste(REFRESH_LAST, "Data has been refreshed", "\n"))
+  })
+  
+  
+  
+  # Shinyserver
+  shinyServer(function(input, output, session) {
+    
+    # Check first run
+    check_firstrun <- reactiveValues(data = TRUE)
+    session$onFlushed(function() {check_firstrun$data <- FALSE}, once = TRUE)
+  
+    
+    # CEO Performance
       
-      rows_to_show <- rev(input$ceo_performance_performance_summary_rows_current)
+      # Data
+      ceo_performance_data <- reactive({
       
-      xlimit <- as.integer(summarise(ceo_performance_data(), max(total_not_home)))
-      data   <- ceo_performance_data()[rows_to_show,]
-
-      
-      data$attempt_user <- gsub(" ", "\r\n", data$attempt_user)
-      data$attempt_user <- factor(data$attempt_user, levels = data$attempt_user) #lock sorting
-      
-      data <- select(data, attempt_user, total_no_property, total_not_home, total_engaged)
-      data <- melt(data, id.vars = c("attempt_user"),
-                   variable.name = "type")
-
-      ggplot(data, aes(x=attempt_user, y=value, fill=type)) +
-        geom_bar(stat="identity", show.legend = FALSE, position = position_dodge())+
-        scale_fill_brewer()+
-        theme_minimal()+
-        theme(axis.title.x=element_blank())+
-        theme(axis.title.y=element_blank())+
-        scale_y_continuous(limits = c(0, xlimit))+
-        coord_flip()
+        ppd_daterange <- input$ceo_performance_date_ctrl
+        ppd_borough   <- input$ceo_performance_borough_ctrl
         
-    })
-    
-    # Table: Performance summary
-    output$ceo_performance_performance_summary <- DT::renderDataTable(
-      rename(select(ceo_performance_data(), attempt_user, total_engaged, total_not_home, total_no_property),
-      CEO=attempt_user, Engaged=total_engaged, "Not at home"=total_not_home, "No property"=total_no_property),
-      options = list(pageLength = 10, searching = FALSE, scrollCollapse = TRUE, bLengthChange = FALSE, bInfo = FALSE, lengthMenu = list(c(10))),
-      server = FALSE
-    )
+        filter_clause <- list(~attempts_date >= ppd_daterange[1], ~attempts_date <= ppd_daterange[2])
+        if (ppd_borough != "All") filter_clause <- append(filter_clause, ~borough == ppd_borough)
+        
+        ppd_filtered <- filter_(PMP_PERFORMANCE_DAILY$data, .dots = filter_clause) #Standard Evaluation
+        
+        ppd_grouped <- group_by(ppd_filtered, attempt_user)
+        
+        ppd_by_user <- summarise(ppd_grouped,
+                                 total_attempts    = sum(total_attempts),
+                                 total_engaged     = sum(total_f2f),
+                                 total_not_home    = sum(total_not_home),
+                                 total_no_property = sum(total_no_proerty_exists),
+                                 rate_engaged      = sum(total_f2f)/sum(total_attempts),
+                                 rate_not_home     = sum(total_not_home)/sum(total_attempts),
+                                 rate_no_property  = sum(total_no_proerty_exists)/sum(total_attempts))
+  
+      })
+      
+      # Chart: Performance Chart
+      output$ceo_performance_performance_chart <- renderPlot({
+        
+        rows_to_show <- rev(input$ceo_performance_performance_summary_rows_current)
+        
+        xlimit <- as.integer(summarise(ceo_performance_data(), max(total_not_home)))
+        data   <- ceo_performance_data()[rows_to_show,]
+  
+        
+        data$attempt_user <- gsub(" ", "\r\n", data$attempt_user)
+        data$attempt_user <- factor(data$attempt_user, levels = data$attempt_user) #lock sorting
+        
+        data <- select(data, attempt_user, total_no_property, total_not_home, total_engaged)
+        data <- melt(data, id.vars = c("attempt_user"),
+                     variable.name = "type")
+  
+        ggplot(data, aes(x=attempt_user, y=value, fill=type)) +
+          geom_bar(stat="identity", show.legend = FALSE, position = position_dodge())+
+          scale_fill_brewer()+
+          theme_minimal()+
+          theme(axis.title.x=element_blank())+
+          theme(axis.title.y=element_blank())+
+          scale_y_continuous(limits = c(0, xlimit))+
+          coord_flip()
+          
+      })
+      
+      # Table: Performance summary
+      output$ceo_performance_performance_summary <- DT::renderDataTable(
+        rename(select(ceo_performance_data(), attempt_user, total_engaged, total_not_home, total_no_property),
+        CEO=attempt_user, Engaged=total_engaged, "Not at home"=total_not_home, "No property"=total_no_property),
+        options = list(pageLength = 10, searching = FALSE, scrollCollapse = TRUE, bLengthChange = FALSE, bInfo = FALSE, lengthMenu = list(c(10))),
+        server = FALSE
+      )
+  
+      # Dynamic option lists
+      observe({
+        c <- generate_choices(PMP_BOROUGHS$data)
+        updateSelectInput(session, "ceo_performance_borough_ctrl", choices = c)
+        cat("updated option list ", c, "\n")
+      })
+      
+  })
 
-    # Dynamic option lists
-    observe({
-      c <- generate_choices(PMP_BOROUGHS$data)
-      updateSelectInput(session, "ceo_performance_borough_ctrl", choices = c)
-      cat("updated option list ", c, "\n")
-    })
-    
-})
+} #db.R
--- ui.R
+++ ui.R
@@ -4,90 +4,99 @@
 #
 
 library(shiny)
+library(markdown)
 library(shinydashboard)
 source("ui_config.R")
 source("ui_functions.R")
 
 
-# Custom CSS
-CustomCss <- tags$head(tags$style(HTML('
-  .dataTables_length {
-    display: none;
-  }
-')))
-
-
-
-# Sidebar
-sidebar <- dashboardSidebar(
-  width = SIDEWIDTH,
-  sidebarMenu(
-    convertMenuItem(menuItem("CEO Performance", tabName = "tab1",
-    
-                             icon = icon("bar-chart"),
-                             dateRangeInput("ceo_performance_date_ctrl", "Filter by Date", start = STARTDATE, format="dd/mm/yyyy"),
-                             selectInput("ceo_performance_borough_ctrl", "Filter by Borough", choices = list("All")),
-                             radioButtons("ceo_performance_view_ctrl", "Switch View", c("Values"= "values", "Ratios" = "ratios"))
-                             #htmlOutput("ceo_performance_borough_filter_server")
-                             ),tabName = "tab1"),
-    
-    convertMenuItem(menuItem("Time Performance", tabName = "tab2",
-                             
-                             icon = icon("clock-o"),
-                             dateRangeInput("time_performance_date_ctrl", "A test control", start = STARTDATE, format="dd/mm/yyyy")                             
-                             ),tabName = "tab2"),
-
-    
-    convertMenuItem(menuItem("Forward Planning", tabName = "tab3",
-                             
-                             icon = icon("calendar"),
-                             dateRangeInput("forward_planning_date_ctrl", "A test control", start = STARTDATE, format="dd/mm/yyyy")                                                          
-                             ),tabName = "tab3")                          
-  )
-)
-
-
-
-
-# Body
-body <- dashboardBody(
-  #CustomCss,
-  tabItems(
-
-    #CEO Performance    
-    tabItem(tabName = "tab1",
-            fluidRow(
-              box(title = "Performance Summary", solidHeader = TRUE, status = "primary", DT::dataTableOutput("ceo_performance_performance_summary"), height=500+62),
-              box(title = "Performance Chart", solidHeader = TRUE, status = "primary", plotOutput("ceo_performance_performance_chart", height=500))
-            )
-    ),
-    # 
-    #Time Performance
-    tabItem(tabName = "tab2"
-
-    ),
-    
-    #Forward Planning
-    tabItem(tabName = "tab3"
-
+# db.R
+if (!DB) {
+  shinyUI(fluidPage(fluidRow(column(3,
+    includeMarkdown("db.md")
+  ))))
+} else {
+  
+  
+  # Custom CSS
+  CustomCss <- tags$head(tags$style(HTML('
+    .dataTables_length {
+      display: none;
+    }
+  ')))
+  
+  
+  
+  # Sidebar
+  sidebar <- dashboardSidebar(
+    width = SIDEWIDTH,
+    sidebarMenu(
+      convertMenuItem(menuItem("CEO Performance", tabName = "tab1",
+      
+                               icon = icon("bar-chart"),
+                               dateRangeInput("ceo_performance_date_ctrl", "Filter by Date", start = STARTDATE, format="dd/mm/yyyy"),
+                               selectInput("ceo_performance_borough_ctrl", "Filter by Borough", choices = list("All")),
+                               radioButtons("ceo_performance_view_ctrl", "Switch View", c("Values"= "values", "Ratios" = "ratios"))
+                               #htmlOutput("ceo_performance_borough_filter_server")
+                               ),tabName = "tab1"),
+      
+      convertMenuItem(menuItem("Time Performance", tabName = "tab2",
+                               
+                               icon = icon("clock-o"),
+                               dateRangeInput("time_performance_date_ctrl", "A test control", start = STARTDATE, format="dd/mm/yyyy")                             
+                               ),tabName = "tab2"),
+  
+      
+      convertMenuItem(menuItem("Forward Planning", tabName = "tab3",
+                               
+                               icon = icon("calendar"),
+                               dateRangeInput("forward_planning_date_ctrl", "A test control", start = STARTDATE, format="dd/mm/yyyy")                                                          
+                               ),tabName = "tab3")                          
     )
-  )  
-)
-
-
-
-
-# Page
-dashboardPage(
-  dashboardHeader(
-    titleWidth = SIDEWIDTH,
-    title = TITLE),
-  sidebar,
-  body
-)
-
-
-
-
-
-
+  )
+  
+  
+  
+  
+  # Body
+  body <- dashboardBody(
+    #CustomCss,
+    tabItems(
+  
+      #CEO Performance    
+      tabItem(tabName = "tab1",
+              fluidRow(
+                box(title = "Performance Summary", solidHeader = TRUE, status = "primary", DT::dataTableOutput("ceo_performance_performance_summary"), height=500+62),
+                box(title = "Performance Chart", solidHeader = TRUE, status = "primary", plotOutput("ceo_performance_performance_chart", height=500))
+              )
+      ),
+      # 
+      #Time Performance
+      tabItem(tabName = "tab2"
+  
+      ),
+      
+      #Forward Planning
+      tabItem(tabName = "tab3"
+  
+      )
+    )  
+  )
+  
+  
+  
+  
+  # Page
+  dashboardPage(
+    dashboardHeader(
+      titleWidth = SIDEWIDTH,
+      title = TITLE),
+    sidebar,
+    body
+  )
+  
+  
+} #db.R  
+  
+  
+  
